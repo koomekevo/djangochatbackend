ef3eeab97125e4ea55fdddaa5679730af3878e79
koomekevo <koome.kevo@gurucrafts.net>
2023-07-28 15:47:30 +0300

    Rename Django app to chat

--- chat/apps.py
+++ chat/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class DjangochatbackendappConfig(AppConfig):
+class ChatConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'djangochatbackendapp'
+    name = 'chat'
