7e81184dd3ba0a25e32e4b4aa9a721154492634f
koomekevo <koome.kevo@gurucrafts.net>
2023-07-28 15:51:49 +0300

    Add User and Message models

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=100, unique=True)
+    # Add other user-related fields
+
+    def __str__(self):
+        return self.username
+
+class Message(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_messages')
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.sender} -> {self.receiver}: {self.content}'
